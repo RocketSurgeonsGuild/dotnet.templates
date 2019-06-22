abbb96f066348cd5e3851c9c528dd9986878f9c0
David Driscoll <david.driscoll@gmail.com>
2019-06-22 16:04:08 -0400

    Added cli template

---
+++ templates/Cli/Default.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Rocket.Surgery.Cli;
+using Rocket.Surgery.Conventions;
+using Rocket.Surgery.Extensions.CommandLine;
+
+[assembly: Convention(typeof(Program))]
+
+namespace Rocket.Surgery.Cli
+{
+    public class Default : IDefaultCommand
+    {
+        private readonly IApplicationState state;
+        private readonly ILogger<Default> logger;
+
+        public Default(IApplicationState state, ILogger<Default> logger)
+        {
+            this.state = state;
+            this.logger = logger;
+        }
+
+        public int Run(IApplicationState state)
+        {
+            Console.WriteLine("Hello World!");
+            return 1;
+        }
+    }
+}
---
+++ templates/Cli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.Hosting;
+using Rocket.Surgery.Cli;
+using Rocket.Surgery.Conventions;
+using Rocket.Surgery.Conventions.Reflection;
+using Rocket.Surgery.Conventions.Scanners;
+using Rocket.Surgery.Extensions.CommandLine;
+using Rocket.Surgery.Extensions.Configuration;
+using Rocket.Surgery.Extensions.DependencyInjection;
+using Rocket.Surgery.Extensions.Logging;
+using Rocket.Surgery.Hosting;
+
+[assembly: Convention(typeof(Program))]
+
+namespace Rocket.Surgery.Cli
+{
+    public class Program : ICommandLineConvention
+    {
+        public static Task Main(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .LaunchWith(RocketBooster.For(DependencyContext.Default))
+                .ConfigureRocketSurgey(builder => { })
+                .RunCli();
+        }
+
+        public void Register(ICommandLineConventionContext context)
+        {
+            context.OnRun<Default>();
+        }
+    }
+}
