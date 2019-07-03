28e3a0e39f7b65a5a711156c4a14fdcb36686cc6
David Driscoll <david.driscoll@gmail.com>
2019-07-03 16:51:22 -0400

    Added function template

---
+++ templates/Function/Function.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Rocket.Surgery.Functions
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILogger<Function> logger) => _logger = logger;
+
+        [FunctionName("Function")]
+        public ActionResult<int> Run([HttpTrigger("get")]HttpRequest context, ILogger logger)
+        {
+            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            return 1;
+        }
+    }
+}
---
+++ templates/Function/Startup.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Rocket.Surgery.Extensions.DependencyInjection;
+using Rocket.Surgery.Hosting.Functions;
+
+[assembly: WebJobsStartup(typeof(Rocket.Surgery.Functions.Startup))]
+
+namespace Rocket.Surgery.Functions
+{
+    public class Startup : IWebJobsStartup, IServiceConvention
+    {
+        public void Register(IServiceConventionContext context)
+        {
+        }
+
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.UseRocketSurgery(
+                this,
+                hostBuilder =>
+                {
+                });
+        }
+    }
+}
